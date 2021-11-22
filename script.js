4156ac67a1eff8611d989c93b4a79c21255fbcc5
baxrom <baxrom>
2021-11-22 23:25:21 +0500

    harflar git status git commit -m harflar

---
+++ script.js
@@ -0,0 +1,24 @@
+let unliXarf = prompt('Xoxlagan matiningizni kiriting', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates dolorem ex, labore vel soluta obcaecati!')
+let str = unliXarf.split('')
+let son = 0
+let unli = ["a", "u", "e", "o", "i"]
+for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < unli.length; j++) {
+        if (unli[j] == str[i]) {
+            son++
+        }
+    }
+
+}
+console.log(`siz kirit gan gapda shuncha unli harflat mavjud ${son}`);
+let son2 = 0
+let unli2 = ["a", "u", "e", "o", "i", " ", ",", "!", "."]
+for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < unli2.length; j++) {
+        if (unli2[j] == str[i]) {
+            son2++
+        }
+    }
+    total = unliXarf.length - son2
+}
+console.log(`siz kirit gan gapda shuncha unli harflat mavjud ${total}`);
